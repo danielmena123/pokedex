9a78f6a66f8de54851d85485b1c9f4b0bfadeabe
dani3lm3na1999 <dani3lm3na@gmail.com>
2022-06-26 20:09:15 -0600

    deploy v 1.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: "https://pokeapi.co/api/v2",
-  usersURL: "https://api-pokedex-backend.herokuapp.com/"
+  usersURL: "https://api-pokedex-backend.herokuapp.com"
 };
